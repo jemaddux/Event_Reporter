7e13550af3bdb20eafc36fdb27d91d2729aca4c3
John Maddux <jemaddux@gmail.com>
2013-02-05 10:02:01 -0700

    Moving to same file

--- bin/command_prompt.rb
+++ bin/command_prompt.rb
@@ -1,7 +1,7 @@
 require 'rainbow'
 require 'csv'
-require_relative 'queue'
-require_relative 'help'
+#require_relative 'queue'
+#equire_relative 'help'
 
 class CommandPrompt
   def initialize
@@ -42,10 +42,66 @@ class CommandPrompt
       else
         #do nothing
       end
-      puts "The input to run_queue was #{input}. The command was #{command}."
   end
 
 end
+
+class Queue
+  def initialize
+    @the_queue = []
+  end
+
+  def new_queue
+    contents = manager.load_file()
+    puts contents
+  end
+
+  def find(input)
+    puts "find" # <attribute> <criteria>"
+      #Load the queue with all records matching the criteria for the given attribute. Example usages:
+      #examples
+      #find zipcode 20011
+      #find last_name Johnson
+      #find state VA
+      #The comparison should:
+      #Be insensitive to case, so "Mary" and "mary" would be found in the same search
+      #Be insensitive to internal whitespace, but not external:
+      #  "John" and "John " are considered matches
+      #  "John Paul" and "Johnpaul" are not matches
+      #Not do substring matches, so a find first_name Mary does not find a record with first name "marybeth"
+  end
+  
+
+  def load_file(filename="event_attendees.csv")
+    begin
+      contents = CSV.open ("static/"+filename.to_s), headers: true, header_converters: :symbol
+      puts contents  
+    rescue
+      puts "Could not find a file named #{filename.to_s}."
+    end
+    return contents    #erase any loaded data and parse the specified file.
+  end
+  
+end
+
+class Help
+  def help(input)
+    #puts the input for some reason???
+    if ("" == input[1..-1].join(" "))
+      puts "The possible commands are: ".color(:red)
+      puts "load <filename> -- help -- queue count -- queue clear -- queue print -- queue print by <attribute> -- queue save to <filename.csv> -- find <attribute> <criteria>"  
+    else
+      long_help
+    end
+    puts ""
+  end
+
+  def long_help(input)
+    puts input
+    puts ""
+    ##needs and explanation of all the commands
+  end
+end  
 
 manager = CommandPrompt.new()
 manager.run
--- lib/help.rb
+++ lib/help.rb
@@ -1,20 +1,2 @@
 require_relative 'command_prompt'
 
-class Help
-  def help(input)
-    #puts the input for some reason???
-    if ("" == input[1..-1].join(" "))
-      puts "The possible commands are: ".color(:red)
-      puts "load <filename> -- help -- queue count -- queue clear -- queue print -- queue print by <attribute> -- queue save to <filename.csv> -- find <attribute> <criteria>"  
-    else
-      long_help
-    end
-    puts ""
-  end
-
-  def long_help(input)
-    puts input
-    puts ""
-    ##needs and explanation of all the commands
-  end
-end  
