433c65b3f312ca28ba2cb846389a100320b1d6f2
John Maddux <jemaddux@gmail.com>
2013-02-05 13:56:35 -0700

    before I split classes for safety

--- bin/command_prompt.rb
+++ bin/command_prompt.rb
@@ -86,7 +86,6 @@ end
 
 class Help
   def help(input)
-    #puts the input for some reason???
     if ("" == input[1..-1].join(" "))
       puts "The possible commands are: ".color(:red)
       puts "load <filename> -- help -- queue count -- queue clear -- queue print -- queue print by <attribute> -- queue save to <filename.csv> -- find <attribute> <criteria>"  
@@ -97,9 +96,31 @@ class Help
   end
 
   def long_help(input)
-    puts input
-    puts ""
-    ##needs and explanation of all the commands
+    case input[1]
+    when 'load' then puts 'load'
+    when 'help' then puts 'help'
+    when 'queue' then queue_help(input)
+    when 'find' then puts 'find'
+      #<attribute> <criteria>
+    else puts "You gotta put something else."
+    end
+    ##### not finished
+  end
+
+  def queue_help(input)
+    case input[2]
+    when "count" then puts "Count help"
+    when "clear" then puts "Clear help"
+    when "print"
+      if input[3] == "by"
+        #print by <attribute>
+      else
+        #print
+      end
+    when "save" then puts "save to <filename.csv>"
+    else puts "something else please"
+    end
+    ###not finished
   end
 end  
 
