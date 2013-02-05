848c726159c2e85b0b3122f1c55faf3cedace593
John Maddux <jemaddux@gmail.com>
2013-02-05 15:20:47 -0700

    queue.find done

--- lib/queue.rb
+++ lib/queue.rb
@@ -17,24 +17,12 @@ class EventQueue
   	puts ""
   	####check to make sure that input[0] is actually a header and if not then provide header options
   	@the_file.each do |line|
-      if line[input[0].to_sym].downcase == input[1] 
-      	puts "#{line}"
+      if line[input[0].to_sym].downcase == input[1]
+      	@the_queue.push(line)
+      	#puts "#{line}"
       end
   	end
-    #puts "find" # <attribute> <criteria>"
-      #Load the queue with all records matching the criteria for the given attribute. Example usages:
-      #examples
-      #find zipcode 20011
-      #find last_name Johnson
-      #find state VA
-      #The comparison should:
-      #Be insensitive to case, so "Mary" and "mary" would be found in the same search
-      #Be insensitive to internal whitespace, but not external:
-      #  "John" and "John " are considered matches
-      #  "John Paul" and "Johnpaul" are not matches
-      #Not do substring matches, so a find first_name Mary does not find a record with first name "marybeth"
-      puts input
-      puts @the_queue = @the_file
+    puts "Find done. #{@the_queue.count} results found. Use 'queue print' to view them."
   end
 
   def load_file(filename="event_attendees.csv")
