d3f2c80bd20245a7fba4b3e607df3d6234b022cc
John Maddux <jemaddux@gmail.com>
2013-02-05 16:45:28 -0700

    redoing the queue load to be [{},{},{}]

--- lib/queue.rb
+++ lib/queue.rb
@@ -5,6 +5,7 @@ require 'rainbow'
 class EventQueue
   def initialize
     @the_queue = new_queue
+    @the_file = []
   end
 
   def new_queue
@@ -25,23 +26,50 @@ class EventQueue
 
   def load_file(filename="event_attendees.csv")
     begin
-      @the_file = CSV.open "lib/"+filename, headers: true, header_converters: :symbol
-      #@the_file.each do |line|
-      #  puts line
-      #end
+      temp_var = CSV.open "lib/"+filename, headers: true, header_converters: :symbol
+      temp_var.each do |line|
+        @the_file.push(line)
+      end
+      puts @the_file
+      a = {x: "12d", y: 12}
+      x = []
+      10.times do |r|
+      	x.push(a)
+      end
+      puts x
     rescue
-      #puts "Could not find a file named #{filename.to_s}."
     end
   end
+#  ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode
+# 1,11/12/08 10:47,Allison,Nguyen,arannon@jumpstartlab.com,6154385000,3155 19th St NW,Washington,DC,20010
+# 2,11/12/08 13:23,SArah,Hankins,pinalevitsky@jumpstartlab.com,414-520-5000,2022 15th Street NW,Washington,DC,20009
+# 3,11/12/08 13:30,Sarah,Xx,lqrm4462@jumpstartlab.com,(941)979-2000,4175 3rd Street North,Saint Petersburg,FL,33703
+# 4,11/12/08 14:04,Jennifer,Cope,bjgielskil@jumpstartlab.com,704-813-3000,1133 Shelton Ave,Nashville,TN,37216
+# 5,11/12/08 14:46,Douglas,Zimmerman,vjeller.79@jumpstartlab.com,425-274-5000,1818 Woodland Ave #101,Des Moines,IA,50309
+
+  def print
+  	puts ""
+  	puts "LAST NAME\tFIRST NAME\tEMAIL\tZIPCODE\tCITY\tSTATE\tADDRESS\tPHONE"
+  	5.times do |x|
+      items2 = @the_queue[x].to_s.split(',')
+      items2.each {|x| puts x}
+      puts ""
+      # items2 = items.join(" ")
+      
+      #puts "#{items[:last_name]}\t#{items[:first_name]}\t#{items[:email_address]}\t#{items[:zipcode]}\t#{items[:city]}\t#{items[:state]}\t#{items[:street]}\t#{items[:homephone]}"
+      
+      # ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode
+  	end
+
+  	#  <--- tab delimited
+  end
+
 end
 
 if __FILE__ == $0
 	quely = EventQueue.new()
   quely.load_file
-  command = "find first_name john".downcase.split(" ")
-  10.times do
-    quely.find(command[1..-1])
-  end
+  #command = "find first_name john".downcase.split(" ")
 end
 
 #puts Dir.pwd   - shows your current directory
