fd2d449d252a54ce285e082b1965d6ccb2e82239
John Maddux <jemaddux@gmail.com>
2013-02-05 14:22:04 -0700

    Working on getting the file to load

--- lib/help.rb
+++ lib/help.rb
@@ -1,2 +1,42 @@
-require_relative 'command_prompt'
+require_relative '../bin/command_prompt'
 
+
+class Help
+  def help(input)
+    if ("" == input[1..-1].join(" "))
+      puts "The possible commands are: ".color(:red)
+      puts "load <filename> -- help -- queue count -- queue clear -- queue print -- queue print by <attribute> -- queue save to <filename.csv> -- find <attribute> <criteria>"  
+    else
+      long_help(input)
+    end
+    puts ""
+  end
+
+  def long_help(input)
+    case input[1]
+    when 'load' then puts 'load'
+    when 'help' then puts 'help'
+    when 'queue' then queue_help(input)
+    when 'find' then puts 'find'
+      #<attribute> <criteria>
+    else puts "You gotta put something else."
+    end
+    ##### not finished
+  end
+
+  def queue_help(input)
+    case input[2]
+    when "count" then puts "Count help"
+    when "clear" then puts "Clear help"
+    when "print"
+      if input[3] == "by"
+        #print by <attribute>
+      else
+        #print
+      end
+    when "save" then puts "save to <filename.csv>"
+    else puts "something else please"
+    end
+    ###not finished
+  end
+end  
--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,63 @@
-require_relative 'command_prompt'
+require_relative '../bin/command_prompt'
+require 'csv'
 
+class EventQueue
+  def initialize
+    @the_queue = new_queue
+  end
 
-quely = Queue.new()
-quely.new_queue
+  def new_queue
+  	@the_queue = []
+  end
+
+  def find(input)
+    puts "find" # <attribute> <criteria>"
+      #Load the queue with all records matching the criteria for the given attribute. Example usages:
+      #examples
+      #find zipcode 20011
+      #find last_name Johnson
+      #find state VA
+      #The comparison should:
+      #Be insensitive to case, so "Mary" and "mary" would be found in the same search
+      #Be insensitive to internal whitespace, but not external:
+      #  "John" and "John " are considered matches
+      #  "John Paul" and "Johnpaul" are not matches
+      #Not do substring matches, so a find first_name Mary does not find a record with first name "marybeth"
+  end
+  
+  def run_queue(input)
+      command = input.split(" ")
+      case command[1]
+      when "count"  
+        #output how many records are in the current queue
+      when "clear"
+        #clear the current queue
+      when "print"
+        #print out a tab-deliminated data table with a headerrow following this format:
+        #LAST NAME  FIRST NAME  EMAIL  ZIPCODE  CITY  STATE  ADDRESS  PHONE
+      when "print" #"by <attribute>"
+        #print the data sorted by the specified atribute like zipcode
+      when "save" #"to <filename.csv>"
+        #Export the current queue to the specified filename as a CSV. The file should should include data 
+        #and headers for last name, first name, email, zipcode, city, state, address, and phone number.
+        #also make sure that the filename ends in csv
+        #saves as output.csv if nothing is selected
+      else
+        #do nothing
+      end
+  end
+
+  def load_file(filename="event_attendees.csv")
+    begin
+      @the_file = CSV.open ("../bin/"+filename.to_s), headers: true, header_converters: :symbol
+      puts @the_file
+    rescue
+      puts "Could not find a file named #{filename.to_s}."
+    end
+  end
+end
+
+if __FILE__ == $0
+	quely = EventQueue.new()
+  quely.load_file
+end
