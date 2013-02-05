a07a74bfaea2d623e53c85ba7b4651d3188086f4
John Maddux <jemaddux@gmail.com>
2013-02-04 22:50:08 -0700

    moving files around

--- lib/queue.rb
+++ lib/queue.rb
@@ -2,10 +2,16 @@ require_relative 'command_prompt'
 
 class Queue
   def initialize
-  	
+  	@the_queue = []
   end
 
-
+  def new_queue
+  	contents = manager.load_file()
+  	puts contents
+  end
 
   
 end
+
+quely = Queue.new()
+quely.new_queue
