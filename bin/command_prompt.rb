b2e30fea4b519e61a4ca8cfd4c311df0c63e352f
John Maddux <jemaddux@gmail.com>
2013-02-05 14:04:16 -0700

    Finished splitting up classes.

--- bin/command_prompt.rb
+++ bin/command_prompt.rb
@@ -1,6 +1,6 @@
 require 'rainbow'
 require 'csv'
-#require_relative '../lib/queue'
+require_relative '../lib/queue'
 require_relative '../lib/help'
 
 class CommandPrompt
@@ -18,69 +18,10 @@ class CommandPrompt
       when "help" then @help.help(command)
       when "queue" then @queue.run_queue(command)
       when "find" then @queue.find(command)
-      when "esc" then break ##For some reason it takes two esc to break the loop.
+      when "esc" then break 
       else puts "Please enter a valid command. Enter 'help' for help or esc to exit."
       end
     end  
-  end
-  
-  def run_queue(input)
-      command = input.split(" ")
-      case command[1]
-      when "count"  
-        #output how many records are in the current queue
-      when "clear"
-        #clear the current queue
-      when "print"
-        #print out a tab-deliminated data table with a headerrow following this format:
-        #LAST NAME  FIRST NAME  EMAIL  ZIPCODE  CITY  STATE  ADDRESS  PHONE
-      when "print" #"by <attribute>"
-        #print the data sorted by the specified atribute like zipcode
-      when "save" #"to <filename.csv>"
-        #Export the current queue to the specified filename as a CSV. The file should should include data 
-        #and headers for last name, first name, email, zipcode, city, state, address, and phone number.
-        #also make sure that the filename ends in csv
-        #saves as output.csv if nothing is selected
-      else
-        #do nothing
-      end
-  end
-
-end
-
-class EventQueue
-  def initialize
-    @the_queue = []
-  end
-
-  def new_queue
-    contents = manager.load_file()
-    puts contents
-  end
-
-  def find(input)
-    puts "find" # <attribute> <criteria>"
-      #Load the queue with all records matching the criteria for the given attribute. Example usages:
-      #examples
-      #find zipcode 20011
-      #find last_name Johnson
-      #find state VA
-      #The comparison should:
-      #Be insensitive to case, so "Mary" and "mary" would be found in the same search
-      #Be insensitive to internal whitespace, but not external:
-      #  "John" and "John " are considered matches
-      #  "John Paul" and "Johnpaul" are not matches
-      #Not do substring matches, so a find first_name Mary does not find a record with first name "marybeth"
-  end
-  
-  def load_file(filename="event_attendees.csv")
-    begin
-      contents = CSV.open ("static/"+filename.to_s), headers: true, header_converters: :symbol
-      puts contents  
-    rescue
-      puts "Could not find a file named #{filename.to_s}."
-    end
-    return contents    #erase any loaded data and parse the specified file.
   end
 end
 
