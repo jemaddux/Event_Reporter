a1fe088102540624cf56df84b461573d152f28f4
John Maddux <jemaddux@gmail.com>
2013-02-05 18:16:31 -0700

    Fixed the loadgit add -Agit add -A

--- lib/queue.rb
+++ lib/queue.rb
@@ -6,6 +6,7 @@ class EventQueue
   def initialize
     @the_queue = new_queue
     @the_file = []
+    @keys = {id: 0, regdate: 1, first_name: 2, last_name: 3, email_address: 4, homephone: 5, street: 6, city: 7, state: 8, zipcode: 9}
   end
 
   def new_queue
@@ -26,26 +27,15 @@ class EventQueue
 
   def load_file(filename="event_attendees.csv")
     begin
-      temp_var = CSV.open "lib/"+filename, headers: true, header_converters: :symbol
+    	puts Dir.pwd
+      temp_var = CSV.open filename, headers: false#, header_converters: :symbol
       temp_var.each do |line|
-        @the_file.push(line)
+        @the_file.push(line)  
       end
-      puts @the_file
-      a = {x: "12d", y: 12}
-      x = []
-      10.times do |r|
-      	x.push(a)
-      end
-      puts x
     rescue
+    	#fail silently
     end
   end
-#  ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode
-# 1,11/12/08 10:47,Allison,Nguyen,arannon@jumpstartlab.com,6154385000,3155 19th St NW,Washington,DC,20010
-# 2,11/12/08 13:23,SArah,Hankins,pinalevitsky@jumpstartlab.com,414-520-5000,2022 15th Street NW,Washington,DC,20009
-# 3,11/12/08 13:30,Sarah,Xx,lqrm4462@jumpstartlab.com,(941)979-2000,4175 3rd Street North,Saint Petersburg,FL,33703
-# 4,11/12/08 14:04,Jennifer,Cope,bjgielskil@jumpstartlab.com,704-813-3000,1133 Shelton Ave,Nashville,TN,37216
-# 5,11/12/08 14:46,Douglas,Zimmerman,vjeller.79@jumpstartlab.com,425-274-5000,1818 Woodland Ave #101,Des Moines,IA,50309
 
   def print
   	puts ""
@@ -69,7 +59,6 @@ end
 if __FILE__ == $0
 	quely = EventQueue.new()
   quely.load_file
-  #command = "find first_name john".downcase.split(" ")
 end
 
 #puts Dir.pwd   - shows your current directory
