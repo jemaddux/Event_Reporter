a1c58b9b700c6eb4c99cd742eb4d9949e8849163
John Maddux <jemaddux@gmail.com>
2013-02-07 23:37:43 -0700

    Fixed find up better

--- lib/queue.rb
+++ lib/queue.rb
@@ -17,36 +17,60 @@ class EventQueue
     count_q
   end
 
-  def find(input) 
-    @queue = []
-
+  def get_key_query(input,number)
     if ((input.length > 2) && (input[0].downcase == "city"))
       key, query = input[0], input[1..-1].join(" ")
     else
       key, query = input[0], input[1]
     end
+    if number == 1
+      return key
+    else
+      return query
+    end
+  end
 
-
+  def little_find(key,query)
     @file.each do |y|
       if y[key.to_sym].to_s.downcase.chomp(" ") == query.to_s.downcase
         @queue.push(y)
       end
     end
+  end
 
-    if (input[2]=="and")
-      if ((input.length > 2) && (input[2].downcase == "city"))
-        key, query = input[2], input[3..-1].join(" ")
-      else
-        key, query = input[3], input[4]
-      end
+  def second_key_query(input,number)
+    if ((input.length > 2) && (input[2].downcase == "city"))
+      key, query = input[2], input[3..-1].join(" ")
+    else
+      key, query = input[3], input[4]
+    end
+    if number == 1
+      return key
+    else
+      return query
+    end
+  end
 
-      temp_queue = @queue
+  def temp_queue_find(key,query)
+    temp_queue = @queue
       @queue = []
       temp_queue.each do |o|
         if o[key.to_sym].to_s.downcase.chomp(" ") == query.to_s.downcase
           @queue.push(o)
         end
       end
+  end
+
+  def find(input) 
+    @queue = []
+    key = get_key_query(input,1)
+    query = get_key_query(input,2)
+    little_find(key,query)
+
+    if (input[2]=="and")
+      key = second_key_query(input,1)
+      query = second_key_query(input,2)
+      temp_queue_find(key,query)
     end
     print_find(key,query)
     puts ""
